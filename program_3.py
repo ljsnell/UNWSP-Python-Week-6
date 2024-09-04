467bbec2e52c7734916d55d5b51c94cec6cb5386
ljsnell <ljsnell7@gmail.com>
2024-09-03 20:34:41 -0500

    Thinking it doesn't make sense to try to unit test program_3 since the open ended piece is the important part of the assignment.

--- program_3.py
+++ program_3.py
@@ -1,5 +1,9 @@
 # Program #3: Tax Rate
-# A retail company must file a monthly sales tax report listing the total sales for the month, and the amount of state and county sales tax collected.  The state sales tax rate is 5 percent and the county sales tax rate is 2.5 percent.  Write a program that asks the user to enter the total sales for the month.  From this figure, the application should calculate and display the following:
+# A retail company must file a monthly sales tax report listing the total sales for the month, 
+# and the amount of state and county sales tax collected. 
+# The state sales tax rate is 5 percent and the county sales tax rate is 2.5 percent.  
+# Write a program that asks the user to enter the total sales for the month.  
+# From this figure, the application should calculate and display the following:
 
 # The amount of county sales tax.
 # The amount of state sales tax.
